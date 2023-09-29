c07854b1f0e56fa7bab1e147fe64980e41a7f5bb
Daniel Bologna <89863727+AbstractBorderStudio@users.noreply.github.com>
2023-09-29 23:29:47 +0200

    Added player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Transform transform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += 1.0f * Vector3.up * Time.deltaTime;
     }
 }
