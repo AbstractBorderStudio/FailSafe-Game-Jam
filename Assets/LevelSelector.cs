1c928a5b8d0daa403207588b508bc7322f3a6d41
Massi327 <115101665+Massi327@users.noreply.github.com>
2023-09-30 19:38:52 +0200

    selezione livelli parzialmente funzionante

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    [SerializeField] List<GameObject> Livelli = new List<GameObject>();
+    [SerializeField] private GameObject currentLiv;
+    [SerializeField] private GameObject nextLiv;
+    [SerializeField] private GameObject prevLiv;
+    [SerializeField] private int contatoreLiv = 0;
+
+    [SerializeField] private Button frecciaDX;
+    [SerializeField] private Button frecciaSX;
+    void Start()
+    {
+        
+        nextLiv = Livelli[1];
+        for (int i = 0; i < Livelli.Count; i++)
+        {
+            if (i==0)
+            {
+                currentLiv = Livelli[i];
+                currentLiv.GetComponent<Animator>().Play("Dentro");
+            }
+
+            if (i==1)
+            {
+                nextLiv = Livelli[i];
+            }
+
+            if (i>0)
+            {
+                Livelli[i].GetComponent<Animator>().Play("FuoriDX");
+            }
+        }
+        
+    }
+
+    void Update()
+    {
+        if (contatoreLiv>0)
+        {
+            frecciaSX.interactable = true;
+        }
+
+        if (contatoreLiv == Livelli.Count-2)
+        {
+            frecciaDX.interactable = true;
+        }
+
+        if (contatoreLiv == 0)
+        {
+            contatoreLiv = 0;
+            frecciaSX.interactable = false;
+        }
+    }
+    
+    public void SetCliccatoSX()
+    {
+        currentLiv.GetComponent<Animator>().SetTrigger("UscitaSX");
+        nextLiv.GetComponent<Animator>().SetTrigger("EntrataDX");
+        prevLiv = currentLiv;
+        currentLiv = nextLiv;
+        contatoreLiv += 1;
+        
+        if (contatoreLiv == Livelli.Count-1)
+        {
+            nextLiv = Livelli[contatoreLiv];
+            frecciaDX.interactable = false;
+        }
+        else 
+        {
+            nextLiv = Livelli[contatoreLiv+1];
+        }
+    }
+
+    public void SetCliccatoDX()
+    {
+        currentLiv.GetComponent<Animator>().SetTrigger("UscitaDX");
+        prevLiv.GetComponent<Animator>().SetTrigger("EntrataSX");
+        nextLiv = currentLiv;
+        currentLiv = prevLiv;
+        contatoreLiv -= 1;
+
+        if (contatoreLiv == 0)
+        {
+            prevLiv = Livelli[0];
+        }
+        else
+        {
+            prevLiv = Livelli[contatoreLiv - 1];
+        }
+    }
+}
